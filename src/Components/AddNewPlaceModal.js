674e741d09bfd03c16cc0676f9cccafec829ec63
Dakota Walker <dakota.a.walker@icloud.com>
2020-03-23 15:33:52 -0400

    Change city to Erie, extend credits.

--- src/Components/AddNewPlaceModal.js
+++ src/Components/AddNewPlaceModal.js
@@ -69,7 +69,7 @@ export function AddNewPlaceModal(props) {
   }
   return (
     <Modal
-      title={<span>Add a New Bay Area Small Business</span>}
+      title={<span>Add a New Erie Small Business</span>}
       visible={props.shouldShow}
       onOk={args => {
         counter === 0 ? handleLinkSubmission() : resetAndClose();
--- src/Components/FAQModal.js
+++ src/Components/FAQModal.js
@@ -45,12 +45,12 @@ function FAQModal(props) {
     {
       title: "What is SaveOurFaves?",
       body:
-        "SaveOurFaves is a directory of Bay Area restaurants and coffee shops that offer online gift cards for purchase. It’s our hope that by providing this resource, we’ll be able to mobilize loyal customers to provide much-needed support for their favorite places in town."
+        "SaveOurFaves is a directory of Erie restaurants and coffee shops that offer online gift cards for purchase. It’s our hope that by providing this resource, we’ll be able to mobilize loyal customers to provide much-needed support for their favorite places in town."
     },
     {
       title: "Why isn’t my favorite business on your site?",
       body:
-        "Please help us add your fave Bay Area food & beverages spots " +
+        "Please help us add your fave Erie food & beverages spots " +
         addPlaceLink("here") +
         ". We're open to adding more small business categories if this catches on."
     },
@@ -60,28 +60,14 @@ function FAQModal(props) {
       body:
         "Delivery and pickup are great options! Some restaurants that don’t normally offer delivery have started to offer curbside pickup during the pandemic so check their websites and social media to see what’s available. <br />" +
         "Tip generously if you can (even for delivery/pickup), since employees are doing extra work and putting their health at risk. <br />" +
-        "Encourage the government to get involved. Sign this " +
-        renderLink("http://chng.it/jM97Sbf9ct", "San Francisco petition") +
-        " to encourage lawmakers to offer emergency small business loans. Please call your US Representative and your Senators. You can be connected to the capitol switchboard at 202-224-3121. Demand that small businesses are part the federal stimulus plan."
-    },
-    {
-      title: "Why is this just for the Bay Area? Can you do this for my city?",
-      body:
-        "As San Francisco natives, we started this project for our community. Over the past days, people have launched similar tools for their specific cities, or even nationally. Check out " +
-        renderLink("https://helpmainstreet.com/", "Help Main Street") +
-        ", " +
-        renderLink("https://givelocal.co/", "GiveLocal") +
-        ", and " +
-        renderLink(
-          "https://rallyforrestaurants.com/",
-          "Rally for Restaurants"
-        ) +
-        "."
+        " Encourage lawmakers to offer emergency small business loans. Please call your US Representative and your Senators. You can be connected to the capitol switchboard at 202-224-3121. Demand that small businesses are part the federal stimulus plan."
     },
     {
       title: "Who built this? And why?",
       body:
-        "We’re Kaitlyn & Mike Krieger -- a husband and wife duo in San Francisco. We’re no longer going out because of COVID-19 (San Francisco is under a “shelter in place” ordinance), so we started buying gift cards to help support our favorite cafes and restaurants during this unpredictable time. SaveOurFaves is our simple way to make it easier for people to help local businesses through this difficult time. We got help and advice from some great friends and local business owners, in particular Phil Levin, Zack Schwab, Kristen Berman, Stefanie Krieger, Melissa Dyrdahl, Laura Buhler, and Paul Einbund. You can contact us with any questions about the site at " +
+        "A San Francisco couple created the application and released the source code for developers around the nation to implement their page locally. Two developers in Erie found the application through their membership at " +
+        renderLink("https://www.radiusco.work/", "Radius Cowork") +
+        " and decided to implement it to do their part. Here's what the creators had to say: We’re Kaitlyn & Mike Krieger -- a husband and wife duo in San Francisco. We’re no longer going out because of COVID-19 (San Francisco is under a “shelter in place” ordinance), so we started buying gift cards to help support our favorite cafes and restaurants during this unpredictable time. SaveOurFaves is our simple way to make it easier for people to help local businesses through this difficult time. We got help and advice from some great friends and local business owners, in particular Phil Levin, Zack Schwab, Kristen Berman, Stefanie Krieger, Melissa Dyrdahl, Laura Buhler, and Paul Einbund. You can contact us with any questions about the site at " +
         renderLink("mailto:info@saveourfaves.org", "info@saveourfaves.org") +
         " and see more about why we decided to start it " +
         renderLink(
@@ -95,7 +81,7 @@ function FAQModal(props) {
     {
       title: "Why isn’t my business showing up in your search results?",
       body:
-        "Please help us add your Bay Area food/beverage business " +
+        "Please help us add your Erie food/beverage business " +
         addPlaceLink("here") +
         ". We're open to adding more small business categories if this catches on."
     },
